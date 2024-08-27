d173460d08ad6e732cf57c20cde309ac6d6b223f
Pranjal Maurya <pranjalmaurya01@gmail.com>
2024-08-27 09:10:51 +0530

    get proxies from api

--- validate_proxy.py
+++ validate_proxy.py
@@ -4,47 +4,58 @@ import threading
 import requests
 
 q = queue.Queue()
-valid_proxies = set()  # Shared set for storing valid proxies
-lock = threading.Lock()  # Lock to synchronize access to the shared set
+valid_proxies = set()
+lock = threading.Lock()
 
-# Read proxies from file and populate the queue
-with open("proxy_list.txt", 'r') as f:
-    proxies = f.read().splitlines()
-    for p in proxies:
-        q.put(p)
+
+def get_proxies(page=1):
+    try:
+        get_proxy_req = requests.get(
+            f"https://proxylist.geonode.com/api/proxy-list?limit=500&page={page}&sort_by=lastChecked&sort_type=desc", timeout=2)
+        get_proxy_req.raise_for_status()
+        for p in get_proxy_req.json()['data']:
+            if 'http' in p['protocols']:
+                ip = f"{p['ip']}:{p['port']}"
+                q.put(ip)
+    except requests.RequestException:
+        return
 
 
 def check_proxy():
-    global q, valid_proxies
     while not q.empty():
         proxy = q.get()
         try:
             r_ip_check = requests.get("https://ipinfo.io/json",
-                                      proxies={'http': proxy, 'https': proxy}, timeout=1)
-        except Exception:
-            continue
-
-        if r_ip_check.status_code == 200:
-            print(f"Working proxy: {proxy}")
-            # Add the valid proxy to the shared set
-            with lock:
-                valid_proxies.add(proxy)
+                                      proxies={'http': proxy}, timeout=1)
+            if r_ip_check.status_code == 200:
+                with lock:
+                    print(proxy)
+                    valid_proxies.add(proxy)
+        except requests.RequestException:
+            return
 
 
-# Create and start 20 threads
-threads = []
-for _ in range(100):
-    thread = threading.Thread(target=check_proxy)
-    threads.append(thread)
-    thread.start()
+if __name__ == '__main__':
+    get_proxies_threads = []
+    page = 1
+    for _ in range(10):
+        thread = threading.Thread(target=get_proxies, args=(page,))
+        get_proxies_threads.append(thread)
+        thread.start()
+        page += 1
+    for t in get_proxies_threads:
+        t.join()
 
-# Wait for all threads to finish
-for thread in threads:
-    thread.join()
+    check_proxy_threads = []
+    for _ in range(100):
+        thread = threading.Thread(target=check_proxy)
+        check_proxy_threads.append(thread)
+        thread.start()
+    for thread in check_proxy_threads:
+        thread.join()
 
-# After all threads are done, write the valid proxies to a file
-with open("working_proxies.txt", 'w') as f:
-    for proxy in valid_proxies:
-        f.write(proxy + "\n")
-
-print("Finished writing valid proxies to file.")
+    if len(valid_proxies) > 20:
+        with open("working_proxies.txt", 'w') as f:
+            for proxy in valid_proxies:
+                f.write(proxy + "\n")
+            print("Finished writing valid proxies to file.")
